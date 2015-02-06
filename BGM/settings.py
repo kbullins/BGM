7c47566387292293b135ad159000379ac1a74f58
kbullins <krb278@nau.edu>
2015-02-06 14:30:41 -0700

    Added Index Template

--- BGM/settings.py
+++ BGM/settings.py
@@ -81,3 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")];
+
+#Template location
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR),"BGM","BGM", "templates"),
+                  )
+
--- BGM/urls.py
+++ BGM/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+# from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'BGM.views.home', name='home'),
+    url(r'^$', 'BGM.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+) 
